b1310c5ba4625c623a8c07c6991a4818df15b554
Aman Tiwari <110708815+Aman20114@users.noreply.github.com>
2023-03-22 17:22:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
-function waitForObject() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const obj = { age: 20, name: "John" };
-      resolve(obj);
-    }, 4000);
-  });
-}
-
-function extractValue(obj) {
-  return obj.age;
-}
-
-function createNewObject(value) {
-  return { message: `Welcome, ${value}. You can vote.` };
-}
-
-const form = document.querySelector("form");
-const ageInput = document.getElementById("age");
-const nameInput = document.getElementById("name");
 const btn = document.getElementById("btn");
 
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  
-  const age = parseInt(ageInput.value);
-  const name = nameInput.value;
+            btn.addEventListener('click', run);
 
-  if (isNaN(age) || name === "") {
-    alert("Please fill in both fields.");
-    return;
-  }
+            // console.log(name, age)
 
-  waitForObject()
-    .then(obj => {
-      if (obj.age >= 18) {
-        alert(createNewObject(name).message);
-      } else {
-        alert(`Oh sorry ${name}. You aren't old enough.`);
-      }
-    })
-    .catch(err => console.log(err));
-});dsdd
+            function checkAge() {
+                const umar = document.getElementById('age').value;
+                const naam = document.getElementById('name').value;
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        if (umar >= 18) {
+                            resolve("Welcome "+ naam +" You can vote")
+                        }
+                        else {
+                            reject("Sorry "+ naam +" You can not vote");
+                        }
+                    }, 4000);
 
+                })
+
+
+            }
+
+           function run(){
+                 checkAge().then((res) => {
+                alert(res);
+            })
+                .catch((err) => {
+                    alert(err);
+                });
+            }
