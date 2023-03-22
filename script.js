155245e304c0bff07b7960c7c510a9ad98bee868
Aman Tiwari <110708815+Aman20114@users.noreply.github.com>
2023-03-22 17:46:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const btn = document.getElementById("btn");
 
-            btn.addEventListener('click', run);
+            form.addEventListener('click', (event) => {
+    event.preventDefault();
 
             function checkAge() {
 				const form = document.querySelector("form");
-                const umar = document.getElementById('age').value;
-                const naam = document.getElementById('name').value;
+                const ageIn = document.getElementById('age').value;
+                const nameIn = document.getElementById('name').value;
                 return new Promise((resolve, reject) => {
                     setTimeout(() => {
                         if (umar >= 18) {
-                            resolve("Welcome "+ naam +". You can vote")
+                            resolve("Welcome "+ nameIn +". You can vote")
                         }
                         else {
-                            reject("Oh sorry "+ naam +". You aren't old enough.");
+                            reject("Oh sorry "+ nameIn +". You aren't old enough.");
                         }
                     }, 4000);
 
